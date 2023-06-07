57f78da18b89a6c7cf724b204694fa6117366fe5
alessandro-farias <alessandro@geinfo.com.br>
2023-06-07 20:05:06 -0300

    Add basic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Router, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HomeModule } from './components/home/home.module';
+import { ListComponent } from './components/list/list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     HomeModule,
-    RouterModule.forRoot([{ path: '', component: HomeComponent }])
+
+    RouterModule.forRoot([{ path: 'home', component: HomeComponent }])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ButtonModule } from 'primeng/button';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { MenuComponent } from '../menu/menu.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     MenuComponent,
-    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ButtonModule
   ],
   providers: [],
 })
